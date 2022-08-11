047b8fa1a36d570dd684a13ecc9e728cdf568c46
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-11 09:49:00 +0300

    Add project files.

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string a = Console.ReadLine();
+
+            if (a.Length < 3)
+                Console.WriteLine("В числе нет третьей цифры");
+            else
+                Console.WriteLine(a[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
